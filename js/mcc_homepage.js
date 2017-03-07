01d0ad6ba1093c56e59de9fd990a8474eb8fa767
Adrian <isfanbogdan@gmail.com>
2017-03-07 14:53:57 +0200

    Added University of change in header

--- js/mcc_homepage.js
+++ js/mcc_homepage.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function(){
 var logoImg = jQuery("<img></img>");
 logoImg.attr("src","https://rawgit.com/SaguaroPrint/saguaroprint.github.io/master/img/logo_min_university.png").css("float","left").css("vertical-align","middle").css("max-height","45px").css("margin-left","10%");
-
+var logoTxt = jQuery("<img></img>");
+logoTxt.attr("src","https://rawgit.com/SaguaroPrint/UnivPortal/master/img/uoc.png").css("float","left").css("vertical-align","middle").css("max-height","45px").css("margin-left","5px");
 //full width Table 
 jQuery("#ENUSmain").css("width","100%");
 
@@ -9,7 +10,9 @@ jQuery("#ENUSmain").css("width","100%");
 jQuery("#ctl00_ImageControlPageHeader").css("max-height","500px").css("min-width","100%").css("flex-shrink","0").css("object-fit","cover");
 jQuery(".NavTop").css("padding","20px 0px").css("font-family","Helvetica Neue").css("font-size","14px").css("color","#333");
 jQuery(".NavTop").css("background","#ffffff");
+jQuery(".NavTop").prepend(logoTxt);
 jQuery(".NavTop").prepend(logoImg);
+
 //jQuery(".NavTop").prepend("<h1> University </h1>").css(".navbar-brand");
 
 //remove DOM elements
