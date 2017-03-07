38cd829fd28d3e286f972c8a63bc0255459d3591
Adrian <isfanbogdan@gmail.com>
2017-03-07 17:53:47 +0200

    Updated header Links and colors

--- js/mcc_homepage.js
+++ js/mcc_homepage.js
@@ -6,10 +6,17 @@ logoTxt.attr("src","https://rawgit.com/SaguaroPrint/UnivPortal/master/img/uoc.pn
 //full width Table 
 jQuery("#ENUSmain").css("width","100%");
 
-//banner customization
+//Banner customization
 jQuery("#ctl00_ImageControlPageHeader").css("max-height","500px").css("min-width","100%").css("flex-shrink","0").css("object-fit","cover");
-jQuery(".NavTop").css("padding","20px 0px").css("font-family","Helvetica Neue").css("font-size","14px").css("color","#333");
 jQuery(".NavTop").css("background","#ffffff");
+jQuery(".NavTop").css("padding","20px 0px").css("font-family","Helvetica Neue,Helvetica,Arial").css("font-size","14px").css("color","#333").css("font-weight","600");
+jQuery(".NavTop").find("a").css("color","#333").css("font-family","Helvetica Neue,Helvetica,Arial").css("font-size","14px").css("text-decoration","none").css("font-weight","600");
+
+//hover function
+jQuery(".NavTop").find("a").hover(function(){jQuery(this).css("color","#d42000").css("border-bottom","2px solid #d42000").css("padding-bottom","3px").css("text-decoration","none");}, function(){jQuery(this).css("color","#333").css("border-bottom","");});
+
+
+//Add logo and Text
 jQuery(".NavTop").prepend(logoTxt);
 jQuery(".NavTop").prepend(logoImg);
 
@@ -24,6 +31,7 @@ jQuery(".spSearch").remove();
 var mccMenuLeft = jQuery(".NavTop > div").children().last().prev();
 var mccMenuRight = jQuery(".NavTop > div").children().last();
 
+//Modify menu CSS
 jQuery(mccMenuLeft).css("float","right").css("display","inline-block").css("margin-right","20px").css("vertical-align","middle").css("text-transform","uppercase").css("margin-top","20px").css("margin-bottom","auto");
 jQuery(mccMenuRight).css("float","").css("display","inline-block").css("margin-right","20px").css("vertical-align","middle").css("text-transform","uppercase").css("margin-top","20px").css("margin-bottom","auto");
 
