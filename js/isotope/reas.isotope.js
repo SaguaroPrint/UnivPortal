4ae6f1fe77cf2d158dd3284fe656d1496a2126b5
bogdanrech <borca.bn@gmail.com>
2017-03-08 16:47:06 +0200

    draft 9

---
+++ js/isotope/reas.isotope.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(function() {
+			jQuery("#catalogContent").css("display", "none");		
+		
+			var fetchItems = [];
+			var inners = ["All"];
+			var filters = ["*"];
+			var obj = getItems();
+			
+			for(var i=0; i < obj.l.length; i++) {
+				filter = obj.l[i].innerHTML;
+				inners.push(filter);
+				filter = filter.replace(/\s/g, '');
+				filters.push(filter);
+				fetchItems.push(getCatalogItems(obj.l[i].id, filter));
+			}
+			
+			var filterCol = newFilter({ filters: filters,	inners: inners});
+			var gridGallery = newGridGallery();
+			var ul = gridGallery.ul;
+			var galleryCol = gridGallery.galleryCol;	
+			var imageGallery = isotopeSection(filterCol, galleryCol);			
+			
+			function getItems(data) {
+				var items;
+				if (data) {
+					var els = jQuery(data);
+					items = jQuery('[id^="ctl00_content_CtlCatalogList_CtlCatalogItemRepeater_ct"]', els);
+				} else {
+					items = jQuery('[id^="ctl00_content_CtlCatalogList_CtlCatalogItemRepeater_ct"]');
+				}
+				var links = items.find('a[id$="SelectName"]');
+				var images = items.find('input[id$="SelectItem"]');
+				var descriptions = items.find('span[id$="Description"]');	
+				return {i: items, l: links, im: images, d: descriptions};
+			}			
+	
+			function getCatalogItems(catalogId, liClass) {
+				theForm.__EVENTTARGET.value = catalogId.replace(/_/g, '$');
+				return jQuery.ajax({
+					type: theForm.method,
+					url: theForm.action,
+					data: $(theForm).serialize(),
+					success: function (data, textStatus, jqXHR) {
+						var products = getItems(data);
+						for(var i=0; i < products.l.length; i++) {
+							var name = products.l[i].innerHTML;
+							var link = products.l[i].href;
+							var description = products.d[i].innerHTML;
+							var image = products.im[i].src;
+							var li = newLi(link ,image, name, description, liClass);		
+							ul.append(li);
+						}
+					},
+					done: function(data) {
+					}
+				});
+			}
+
+			function removeOriginalCatalog() {
+				var catalogContent = jQuery('#catalogContent');
+				catalogContent.remove();
+			}
+			
+			jQuery.when.apply(null, fetchItems).done(function() {
+				jQuery("body").append(imageGallery);
+				loadIsotope();
+				jQuery("#catalogContent").remove();
+			});		
+	})
