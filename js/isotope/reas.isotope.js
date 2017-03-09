0b8aa57dc1ab287919a43f60988a3c87f069de7e
bogdanrech <borca.bn@gmail.com>
2017-03-09 16:02:07 +0200

    draft 26

--- js/isotope/reas.isotope.js
+++ js/isotope/reas.isotope.js
@@ -80,6 +80,6 @@ jQuery(document).ready(function() {
 				var d = new Date();
 				d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
 				var expires = "expires=" + d.toUTCString();
-				document.cookie = "reas=" + json ";" + expires + ";path=/";
+				document.cookie = "reas=" + json + ";" + expires + ";path=/";
 			});		
 	})
