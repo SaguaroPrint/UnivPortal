3922bda60b210847ef29909eefe16ee3660784c5
bogdanrech <borca.bn@gmail.com>
2017-03-09 15:44:02 +0200

    draft 25

--- js/isotope/reas.isotope.js
+++ js/isotope/reas.isotope.js
@@ -48,7 +48,7 @@ jQuery(document).ready(function() {
 							var description = products.d[i].innerHTML;
 							var image = products.im[i].src;
 							var li = newLi(js ,image, description, name, liClass);		
-							var item = "{ \"name\":\"" + name + "\",\"description\":\"" + description + "\",\"js\":\"" + js + "\",\"image\":\"" + image + "\"} " + ((i == (products.l.length - 1))?"":",");
+							var item = "{ \"name\":\"" + name + "\",\"description\":\"" + description + "\",\"js\":\"" + js.replace(/\"/g, '\'') + "\",\"image\":\"" + image + "\"} " + ((i == (products.l.length - 1))?"":",");
 							window.reasJSON += item;
 							ul.append(li);
 						}
@@ -77,8 +77,6 @@ jQuery(document).ready(function() {
 				var json = window.reasJSON;
 				json = json.slice(0, -1);
 				json += "]}";
-				if (this.reasJSON == "") {
-					document.cookie = "reas=" + json;
-				}
+				document.cookie = "reas=" + json;
 			});		
 	})
