2228fb490e5d9f7484d2eb63c1cf5e6e6c3d95fb
icrivat <crivat.iuly@gmail.com>
2017-03-09 17:12:23 +0200

    14

--- js/mcc_homepage.js
+++ js/mcc_homepage.js
@@ -88,4 +88,14 @@ jQuery(document).ready(function() {
     var result = document.evaluate('//*[@id="ctl00_content_CtlAddToCart_InteractivityContainer_panelInteractivity"]/table/tbody/tr[1]/td[2]', document, null, 5, null);
     var el = result.iterateNext();
     el.style.width = "5%";
+
+    // remove white space at the bottom
+    result = document.evaluate('//*[@id="CtlCart"]/tbody/tr/td/table/tbody/tr/td[1]/div[8]', document, null, 5, null);
+    el = result.iterateNext();
+    el.style.visibility = "hidden";
+
+    result = document.evaluate('//*[@id="CtlCart"]/tbody/tr/td/table/tbody/tr/td[1]/br[3]', document, null, 5, null);
+    el = result.iterateNext();
+    el.style.visibility = "hidden";
+
 });
