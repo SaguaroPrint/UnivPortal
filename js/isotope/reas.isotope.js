d4e956f27cb283f5cf264801976ae8f1d1427ed1
bogdanrech <borca.bn@gmail.com>
2017-03-10 18:13:03 +0200

    draft 26

--- js/isotope/reas.isotope.js
+++ js/isotope/reas.isotope.js
@@ -1,49 +1,108 @@
 	
+	
 	jQuery(window).on('load', function() {
 	
 			function getReasCookies() {
+				var reasCookies = {};
 				var cookie = document.cookie;
 				var pairs = cookie.split(";");
 				var i = 0;
-				while (i < pairs.length) {
+				for (var i =0; i< pairs.length; i++) {
 					var pair = pairs[i].split("=");
 					var name = pair[0];
-					if (!name.startsWith("reas")) {
-						pairs.splice(i, 1);
-					} else {
-						i++;
+					if (name.charAt(0) == " ") {
+						name = name.substring(1, name.length);
+					}
+					if (name.startsWith("reas")) {
+						reasCookies[name] = pair[1];
 					}
 				}
-				return pairs;
+				return reasCookies;
 			}
 			
 			var c = getReasCookies();
 			if (c["reasCatalog"]) {
+				var catalog = JSON.parse(unescape(c["reasCatalog"]));
+				var filterCol = newFilter(catalog);
+				var gridGallery = newGridGallery();
+				var ul = gridGallery.ul;
+				var galleryCol = gridGallery.galleryCol;	
+				var imageGallery = isotopeSection(filterCol, galleryCol);
+				
+				/*
+reasCatalog	{
+	"catalog": [{
+		"PromoPoster": "Promo Poster"
+	}, {
+		"LuggageTags": "Luggage Tags"
+	}]
+}
+
+
+reasCategoryMarketingAssets	{
+	"items": ["Contest Postcard", "Downloadable Logo", "Company brochure"]
+}
+
+
+reasProductBusinessCard {
+
+	"name": "Business Card",
+	"description": "It's a business card",
+	"js": "location.href='https://eu.marcomcentral.app.pti.com/Ricoh_EU_Training/Ungureanu_Travel/addToCart.aspx?uigroup_id=4352&product_id=7&node_id=1714';return false;",
+	"image": "https://images-eu.marcomcentral.app.pti.com/printonelogos/images/55/103/thumbnails/1690/BusCard_Preview.png"
+
+
+}
+*/
+				for (var category in catalog) {
+					if (category != "*") {
+						var products = JSON.parse(unescape(c["reasCategory" + category]));
+						for (var i = 0; i < products.items.length; i++) {
+							var product = JSON.parse(unescape(c[products.items[i]]));
+							var li = newLi(product.js ,product.image, product.description, product.name, category);	
+							ul.append(li);
+						}
+					}
+				}
+				
+				var result = document.evaluate('//*[@id="ENUSmain"]/tbody/tr/td/div[1]', 
+					document, null, 5, null);
+				var el = result.iterateNext();
+	
+				jQuery(el).append(imageGallery);
+				jQuery("#catalogContent").remove();
+				loadIsotope();
+				document.getElementById('ENUSmain').style.visibility = 'visible';
+				
 				return;
 			} 
 	
-			
-
 			var fetchItems = [];
-			var inners = ["ALL"];
-			var filters = ["*"];
-			window.reasProducts = [];
-			window.reasCategories = [];
+			var filters = {}
+			filters["*"] = "ALL";
 			
+/*
+reasCatalog {
+	"MarketingAssets": "Marketing Assets",
+	"Identity": "Identity",
+	"LabTime": "Lab Time",
+	"JobSubmission": "Job Submission"
+}
+*/			
 			var obj = getItems();
-			var reasCatalog = "{\"catalog\": [ ";
+			var reasCatalog = "{\"*\":\"ALL\"";
 			for(var i=0; i < obj.l.length; i++) {
-				filter = obj.l[i].innerHTML;
-				inners.push(filter);
-				reasCatalog += "\"" + filter + "\"" + ((i == (obj.l.length - 1))?"":",");
-				filter = filter.replace(/\s/g, '');
-				filters.push(filter);
-				fetchItems.push(getCatalogItems(obj.l[i].id, filter, inners[i+1]));
+				var category = obj.l[i].innerHTML;
+				var inner = category;
+				var filter = category.replace(/\s/g, '');
+				filters[filter] = inner;
+				reasCatalog += ",\"" + filter + "\":\"" + inner + "\"";
+				fetchItems.push(getCatalogItems(obj.l[i].id, filter, inner));
 			}
-			reasCatalog += "]}";
-			window.reasCatalog = "reasCatalog=" + JSON.stringify(reasCatalog);
+			reasCatalog += "}";
+			document.cookie = "reasCatalog=" + escape(reasCatalog);
 			
-			var filterCol = newFilter({ filters: filters,	inners: inners});
+			var filterCol = newFilter(filters);
 			var gridGallery = newGridGallery();
 			var ul = gridGallery.ul;
 			var galleryCol = gridGallery.galleryCol;	
@@ -63,28 +122,28 @@
 				return {i: items, l: links, im: images, d: descriptions};
 			}			
 	
-			function getCatalogItems(catalogId, liClass, category) {
+			function getCatalogItems(catalogId, filter, category) {
 				theForm.__EVENTTARGET.value = catalogId.replace(/_/g, '$');
 				return jQuery.ajax({
 					type: theForm.method,
 					url: theForm.action,
 					data: $(theForm).serialize(),
 					success: function (data, textStatus, jqXHR) {
-						var reasCategory = "{ \"category\":\"" + category + "\", \"items\":[ ";
+						var reasCategory = "{ \"items\":[ ";
 						var products = getItems(data);
 						for(var i=0; i < products.l.length; i++) {
 							var name = products.l[i].innerHTML;
 							var js = jQuery(products.l[i]).attr("onclick");
 							var description = products.d[i].innerHTML;
 							var image = products.im[i].src;
-							var li = newLi(js ,image, description, name, liClass);		
+							var li = newLi(js ,image, description, name, filter);		
 							var item = "{ \"name\":\"" + name + "\",\"description\":\"" + description + "\",\"js\":\"" + js.replace(/\"/g, '\'') + "\",\"image\":\"" + image + "\"} ";
-							window.reasProducts.push("reasProduct" + name.replace(/\s/g, '') + "=" + JSON.stringify(item));
-							reasCategory += "\"" + name + "\"" + ((i == (products.l.length - 1))?"":",");
+							document.cookie = "reasProduct" + name.replace(/\s/g, '') + "=" + escape(item);
+							reasCategory += "\"reasProduct" + name.replace(/\s/g, '') + "\"" + ((i == (products.l.length - 1))?"":",");
 							ul.append(li);
 						}
 						reasCategory += "]}";
-						window.reasCategories.push("reasCategory" + category.replace(/\s/g, '') + "=" + JSON.stringify(reasCategory));
+						document.cookie = "reasCategory" + filter + "=" + escape(reasCategory);
 					},
 					done: function(data) {
 					}
@@ -103,14 +162,8 @@
 				var el = result.iterateNext();
 	
 				jQuery(el).append(imageGallery);
+				jQuery("#catalogContent").remove();
 				loadIsotope();
 				document.getElementById('ENUSmain').style.visibility = 'visible';
-				document.cookie = window.reasCatalog;
-				for (var i = 0; i < window.reasCategories.length; i++) {
-					document.cookie = window.reasCategories[i];
-				}
-				for (var i = 0; i < window.reasProducts.length; i++) {
-					document.cookie = window.reasProducts[i];
-				}
 			});		
-	})	
+	})
