0e23999cb8997632c132fce4fdb3221b64d3de8e
icrivat <crivat.iuly@gmail.com>
2017-03-23 14:14:10 +0200

    change catalog filter

--- js/isotope/create.js
+++ js/isotope/create.js
@@ -1,15 +1,15 @@
 
 	/****
 	**		Create a new item in collection
-	**	@params: 
+	**	@params:
 	**		link -> what to open
-	**		icon type -> icon-video, 
-	**			icon-browser, 
+	**		icon type -> icon-video,
+	**			icon-browser,
 	**			icon-camera,
 	**			icon-attachment...
 	**	@returns:
 	**		li item to be added to the gallery
-	**/			
+	**/
 
 
 	function newLi(js, image, textP, textA, filter) {
@@ -26,54 +26,54 @@
 		var div = jQuery("<div></div>").addClass("gallery-img");
 		var divA = jQuery("<a></a>").attr("onclick", js);
 		var aImg = jQuery("<img></img>").attr("src", image).addClass("reas-img");
-		
+
 		//
 		divA.append(aImg);
 		div.append(divA);
-		
+
 		//**
 		var figure = jQuery("<figure></figure>");
 		//
 		figure.append(div);
 		figure.append(caption);
-		
+
 		//**
 		var li = jQuery("<li></li>").addClass("html " + filter);
 		li.append(figure);
 		return li;
 	}
-	
+
 	/****
 	**		Create gallery holder
 	**	@returns object:
 	**		ul holder for lis
 	**		gallery holder
 	**
-	**/	
-	
+	**/
+
 	function newGridGallery() {
 		var ul = jQuery("<ul></ul>").addClass("grid masonry-items");
 		var divContent = jQuery("<div></div>").addClass("tab-content");
 		var galleryCol =jQuery("<div></div>").addClass("col-md-12 grid-gallery overflow-hidden");
-		galleryCol.attr("id", "reas-galleryCol");
-		
+		galleryCol.attr("id", "reas-galleryCol").css("padding-left", "30px");
+
 		divContent.append(ul);
 		galleryCol.append(divContent);
 		return {ul: ul, galleryCol: galleryCol};
 	}
-	
+
 	/****
 	**		Create gallery filter collection
-	**	@params: 
+	**	@params:
 	**		dataFilter object
 	**		{
 	**			filters: ["*", ".id1", ".id2", ...],
 	**			inners: ["All", "Magento", "Jquery", ...]
 	**		}
-	**	@returns:	
+	**	@returns:
 	**		divCol holder for the ul pool
-	**/	
-	
+	**/
+
 	function newFilter(dataFilter) {
 		var ul = jQuery("<ul></ul>").addClass("portfolio-filter nav nav-tabs nav-tabs-light wow fadeInUp");
 		ul.attr("id", "reas-ul-filter");
@@ -89,48 +89,38 @@
 				li.addClass("active");
 			}
 			li.append(a);
-			ul.append(li);	
+			ul.append(li);
 		}
-		
 
-		
+
+
 		//**
-		var divCenter = jQuery("<div></div>").addClass("text-center");
+		var divCenter = jQuery("<div></div>").addClass("text-center").attr("id", "ulFilterHolder");
 		var filterCol = jQuery("<div></div>").addClass("col-md-12 text-center");
 		divCenter.append(ul);
 		filterCol.append(divCenter);
-		
+
 		return filterCol;
 	}
-	
+
 	/****
 	**		Create gallery main section
-	**	@params: 
+	**	@params:
 	**		filterCol and galleryCol, two divs
 	**		both divs are added to the section
-	**	@returns:	
+	**	@returns:
 	**		the section
-	**/		
-	
+	**/
+
 	function isotopeSection(filterCol, galleryCol) {
 		var divRow = jQuery("<div></div>").addClass("row");
 		var divContainer = jQuery("<div></div>").addClass("container");
 		var section = jQuery("<section></section>").addClass("work-5col gutter work-with-title no-margin-top content-section");
 		section.css("padding" , "40px 0px");
-		
+
 		divRow.append(filterCol);
 		divRow.append(galleryCol);
 		divContainer.append(divRow);
 		section.append(divContainer);
-		return section;		
+		return section;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
