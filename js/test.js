1721f7ee67a2c4c51934cd04a0023baadfb86b2d
colcherica <casiancolcher@gmail.com>
2017-04-13 11:25:07 +0300

    testjs

---
+++ js/test.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+	jQuery("#ctl00_content_ForgotPassword_btnSubmit").removeClass().addClass("btn btn-primary");
+	});
+
+jQuery(".CtlSearchGroup").removeClass().addClass(".test");
